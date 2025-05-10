75e9131032c390f73e42f7ce51768fa47bb8bf15
Neenick <y.j.van.maanen@student.rug.nl>
2025-05-10 11:19:01 +0200

    Added image cropping

--- project_name/preprocessing.py
+++ project_name/preprocessing.py
@@ -9,18 +9,18 @@ dataset_pth = '/Users/yanni/OneDrive/Documenten/Datasets/TT100K/tt100k_2021/tt10
 with open(dataset_pth + 'train/ids.txt') as f:
     for id in f:
         id = id[:-1] # Removes \n at the end of each id. Neither replace, strip, nor rstrip were working.
-        img_path = 'train/' + str(id) + '.jpg'
-        image = io.imread(dataset_pth + img_path)
-        
+        image = io.imread(dataset_pth + 'train/' + str(id) + '.jpg')
+        plt.imshow(image)
+
         # Grayscaling & Normalizing (Normalized within rgb2gray function)
         i, (im1) = plt.subplots(1)
         i.set_figwidth(5)
         gray_image = skimage.color.rgb2gray(image)
-        plt.imshow(gray_image, cmap = 'gray')
+        # plt.imshow(gray_image, cmap = 'gray')
 
         # Resizing from 2048x2048 to 512x512
         resized_image = resize(gray_image, (512, 512), anti_aliasing=True) # Not sure if we're able to scale down further
-        plt.imshow(resized_image, cmap = 'gray')
+        # plt.imshow(resized_image, cmap = 'gray')
 
         # Open annotations file
         with open(dataset_pth + 'annotations_all.json') as json_data:
@@ -33,18 +33,15 @@ with open(dataset_pth + 'train/ids.txt') as f:
             # Loop over Traffic Signs and print their type & bounding box
             for i in range(traffic_signs):
                 bbox = annotations["imgs"][str(id)]["objects"][i]["bbox"]
-                xmin = bbox["xmin"] / 4 # Divide by 4 to account for resizing (2048 --> 512)
-                ymin = bbox["ymin"] / 4
-                xmax = bbox["xmax"] / 4
-                ymax = bbox["ymax"] / 4
+                xmin = int(bbox["xmin"] / 4) # Divide by 4 to account for resizing (2048 --> 512)
+                ymin = int(bbox["ymin"] / 4)
+                xmax = int(bbox["xmax"] / 4)
+                ymax = int(bbox["ymax"] / 4)
                 sign_type = annotations["imgs"][str(id)]["objects"][i]["category"]
                 print(f"Traffic Sign Type: {sign_type}")
                 print(f"xmin: {xmin}\nymin: {ymin}\nxmax: {xmax}\nymax: {ymax}")
+
+        # Cropping the image with the boundary boxes just to see what they look like and how it works
+        cropped_image = resized_image[ymin:ymax, xmin:xmax]
+        plt.imshow(cropped_image, cmap = 'gray')
         plt.show()
-
-        
-
-
-
-
-
