0f6bb42ab4cfafd54cbe3ada53d5dc43b75005ce
david <DavidvanWuykhuyse@gmail.com>
2025-05-23 18:33:17 +0200

    add evaluation.py containing accuracy
    perfomed about 80% on cropped sign images

--- project_name/evaluation.py
+++ project_name/evaluation.py
@@ -1,7 +1,6 @@
 """
-Dataset folder should be located under
-Applied-Machine-Learning/project_name/data/tt100k_2021.
-Make sure this is the case, otherwise it will not work.
+New evaluation file that loads a model trained through main.py
+and uses an accuracy metric to evaluate the performance.
 """
 import os
 import json
@@ -34,12 +33,10 @@ def matplotlib_imshow(img, one_channel=False):
 # directory, it should theoretically work.
 root = os.getcwd() + '/project_name/data/tt100k_2021/'
 annotations = root + 'annotations_all.json'
-filtered_annotations = root + 'filtered_annotations.json'
-ids_file = root + '/train/ids.txt'
+filtered_annotations = root + 'filtered_test_annotations.json'
+ids_file = root + '/test/ids.txt'
 model_path = os.getcwd() + '/project_name/models/model.pth'
 
-# Find all training ids from the ids.txt file in train/
-# This is bound to change as custom splits will be needed.
 if not os.path.exists(filtered_annotations):
     print('Creating a new .json file for training ids.')
     with open(ids_file, 'r') as f:
@@ -71,66 +68,44 @@ transforms = transforms.Compose([
     transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
 ])
 
+# Load testing data
 tt100k_data = TT100KSignDataset(filtered_annotations, root, transforms)
-t_size = int(0.8 * len(tt100k_data))
-v_size = len(tt100k_data) - t_size
-
-# Initialize data loaders for testing and validations sets
-train_split, val_split = random_split(tt100k_data, [t_size, v_size])
-t_loader = DataLoader(train_split, 32, shuffle=True)
-v_loader = DataLoader(val_split, 32, shuffle=True)
-
-# Initialize training loop parameters
-epochs = 1
-lr = 0.001
+test_loader = DataLoader(tt100k_data, 32, shuffle=True)
 
 # Initialize model, optimizer etc.
 print(f'Cuda (GPU support) available: {torch.cuda.is_available()}')
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 num_of_classes = len(tt100k_data.annotations['types'])
+
+# Loading a model:
 model = CNNClassifier(num_of_classes)
 model = model.to(device)
-loss_fn = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=lr)
+m_state_dict = torch.load(model_path, weights_only=True)
+model.load_state_dict(m_state_dict)
 
-for epoch in range(epochs):
-    print(f'Epoch [{epoch + 1}/{epochs}]')
-    # Set model to training mode
-    model.train()
-    running_tloss = 0.0
-    running_vloss = 0.0
+# Dumb testing thing:
+dataiter = iter(test_loader)
+images, labels = next(dataiter)
+img_grid = torchvision.utils.make_grid(images)
+matplotlib_imshow(img_grid, one_channel=True)
+plt.show()
+outputs = model(images)
+_, predicted = torch.max(outputs, 1)
+print(f'Ground truth: {[tt100k_data.idx_to_label[label.item()] for label in labels]}')
+print(f'Predicted: {[tt100k_data.idx_to_label[pred.item()] for pred in predicted]}')
 
-    for i, data in enumerate(tqdm(t_loader)):
-        inputs, labels = data
-        inputs = inputs.to(device)
-        labels = labels.to(device)
-        optimizer.zero_grad()
-        outputs = model(inputs)
-        # img_grid = torchvision.utils.make_grid(inputs)
-        # matplotlib_imshow(img_grid, one_channel=True)
-        # plt.show()
-        t_loss = loss_fn(outputs, labels)
-        t_loss.backward()
-        running_tloss += t_loss.item()
-        optimizer.step()
+# Accuracy:
+correct = 0
+total = 0
 
-        if i % 10 == 0:
-            print(f'\n batch {i}: last loss: {running_tloss / 10}')
-            running_tloss = 0
+with torch.no_grad():
+    for data in tqdm(test_loader):
+        images, labels = data
+        images = images.to(device)
+        labels = labels.to(labels)
+        outputs = model(images)
+        _, pred = torch.max(outputs, 1)
+        total += labels.size(0)
+        correct += (pred == labels).sum().item()
 
-    # Set model to evaluation mode
-    model.eval()
-
-    with torch.no_grad():
-        for i, data in enumerate(tqdm(v_loader)):
-            inputs, labels = data
-            inputs = inputs.to(device)
-            labels = labels.to(device)
-            outputs = model(inputs)
-            loss = loss_fn(outputs, labels)
-            running_vloss += loss
-        avg_loss = running_vloss / len(v_loader)
-        print(f'Average validation loss: {avg_loss:.4f}')
-
-torch.save(model.state_dict(), model_path)
-print(f'Saved model to: {model_path}')
+print('Accuracy on testing data:', correct / total * 100)
