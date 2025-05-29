524da141b89d4a9e8da56f4446d18ba64a910b92
david <DavidvanWuykhuyse@gmail.com>
2025-05-29 13:51:38 +0200

    rewrite localization base model

--- project_name/models/localization_base_model.py
+++ project_name/models/localization_base_model.py
@@ -1,28 +1,26 @@
 import torch.nn as nn
 import torch.nn.functional as F
-import torch
+import torchvision.models as models
 
 
-class cnn_model(nn.Module):
-    def __init__(self):
-        super(cnn_model, self).__init__()
+class BboxRegression(nn.Module):
+    def __init__(self, num_boxes):
+        super(BboxRegression, self).__init__()
+        resnet = models.resnet50(pretrained=True)
+        self.base_model = nn.Sequential(*list(resnet.children())[:-2])
 
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=5, stride=1, padding=0)   # -> (B,32,60,60)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=5, stride=1, padding=0)  # -> (B,64,26,26)
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=5, stride=1, padding=0) # -> (B,128,9,9)
+        for param in self.base_model.parameters():
+            param.requires_grad = False
 
-        self.fc1 = nn.Linear(128 * 9 * 9, 2046)
-        self.fc2 = nn.Linear(2046, 4)
+        self.num_boxes = num_boxes
+        self.bbox_head = nn.Sequential(
+            nn.AdaptiveAvgPool2d((1, 1)),
+            nn.Flatten(),
+            nn.Linear(2048, num_boxes * 4)
+        )
 
-    def forward(self, val):
-        val = F.relu(self.conv1(val))       # [B, 32, 60, 60]
-        val = F.max_pool2d(val, 2, 2)       # [B, 32, 30, 30]
-        val = F.relu(self.conv2(val))       # [B, 64, 26, 26]
-        val = F.max_pool2d(val, 2, 2)       # [B, 64, 13, 13]
-        val = F.relu(self.conv3(val))       # [B, 128, 9, 9]
-
-        val = val.view(val.size(0), -1)     # [B, 10368]
-        val = F.dropout(F.relu(self.fc1(val)), p=0.5, training=self.training)
-        val = torch.sigmoid(self.fc2(val))  # [B, 4] bounding boxes in [0,1] format
-        return val
-
+    def forward(self, x):
+        x = self.base_model(x)
+        x = self.bbox_head(x)
+        x = x.view(x.size(0), -1, 4)
+        return x
